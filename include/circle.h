4defca3f1f68a82c292751d7c236bcebe358b8be
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-02 18:56:50 +0300

    Update circle.h

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,35 @@
 // Copyright 2022 UNN-CS
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
+
 #include <cstdint>
 
+class Circle {
+ private:
+    double radius;
+    double ference;
+    double area;
 
-#endif  // INCLUDE_CIRCLE_H_
+    void calculateFromRadius();
+
+    void calculateFromFerence();
+
+    void calculateFromArea();
+
+ public:
+    explicit Circle(double r);
+
+    void setRadius(double r);
+
+    void setFerence(double f);
+
+    void setArea(double a);
+
+    double getRadius() const;
+
+    double getFerence() const;
+
+    double getArea() const;
+};
+
+#endif // INCLUDE_CIRCLE_H_
