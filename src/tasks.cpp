c0210193ea3dbe25e09b73a1bf6f5851e041a271
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-02 19:00:16 +0300

    Create tasks.cpp

---
+++ src/tasks.cpp
@@ -0,0 +1,30 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_CIRCLE_H_
+#define INCLUDE_CIRCLE_H_
+
+#include <cstdint>
+
+class Circle {
+ private:
+    double radius;
+    double ference;
+    double area;
+
+    void calculateFromRadius();
+    void calculateFromFerence();
+    void calculateFromArea();
+
+ public:
+    explicit Circle(double r);
+
+    void setRadius(double r);
+    void setFerence(double f);
+    void setArea(double a);
+
+    double getRadius() const;
+    double getFerence() const;
+    double getArea() const;
+};
+
+
+#endif  // INCLUDE_CIRCLE_H_
