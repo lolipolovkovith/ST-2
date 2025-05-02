6046ff74c1c9f9b7234de0a31a54fa302a965095
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-02 19:14:33 +0300

    Create tasks.h

---
+++ include/tasks.h
@@ -0,0 +1,9 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+double calculateRopeGap(double earthRadius);
+
+double calculatePoolCost(double poolRadius, double pathWidth);
+
+#endif  // INCLUDE_TASKS_H_
